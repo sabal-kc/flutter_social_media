8464615a7456825093ce224c1a678c2f854cfebd
regmirabindra <073bct531.rabindra@pcampus.edu.np>
2020-04-10 18:09:39 +0545

    fixed sabal mistake

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,6 +1,6 @@
 class Constants {
-  static const String BASE_URL = "http://192.168.1.16:5000/api/";
-  static const String IMAGE_URL = "http://192.168.1.16:5000/";
+  static const String BASE_URL = "http://192.168.0.5:5000/api/";
+  static const String IMAGE_URL = "http://192.168.0.5:5000/";
 
   //ROUTES
 
